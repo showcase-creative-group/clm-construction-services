adad162bdc63bb7b9112806f67815003b1c07409
R.J. Grote <ryan@rjgrote.com>
2025-12-08 23:32:57 +0000

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
